9dace2b9501ddf355fb4f1f63cd049f2811eb18e
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-15 13:56:41 +0200

    Add ESP8266 and make timer with Timer/Counter 2
    I change from Timer/Counter0 to Timer/Counter2, because i dont want to work with tone() and i dont want to change delay, millis and micros function functionality

--- weatherStation/weatherStation.ino
+++ weatherStation/weatherStation.ino
@@ -1,43 +1,102 @@
+//Variabile pentru ESP8266 
+String ssid     = "Simulator Wifi";    // SSID 
+String password = "";           //WiFi-ul virtual nu are o parola prestabilita in ThinkerCAD
+String host     = "api.thingspeak.com"; //API
+const int httpPort   = 80;        //Portul pe care se trimit datele
+String tempURL     = "/update?api_key=ZOPP08IZU17W667D&field2=0"; 
+
+
 //Contorul pentru secunda
-int timerCounter = 0;
+volatile int timerCounter = 0;
 
+int setupESP8266(void) {
+  //Pornirea comunicatiei seriale pe ESP8266
+  Serial.begin(115200);  
+  Serial.println("AT");   
+  delay(10);
+  if (!Serial.find("OK")) return 1;
+    
+  // Conectarea circuitului la WiFI prin SSID si parola
+  Serial.println("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"");
+  delay(10);        //Adaugam un delay pentru ca ESP8266 sa aiba timp sa raspunda
+  if (!Serial.find("OK")) return 2;
+  
+  //Deschiderea conexiunii TCP la gazda
+  Serial.println("AT+CIPSTART=\"TCP\",\"" + host + "\"," + httpPort);
+  delay(50);        //Adaugam un delay pentru ca ESP8266 sa aiba timp sa raspunda
+  if (!Serial.find("OK")) return 3;
+  
+  return 0;
+}
 
-void setup() {
+void readTemp(void) {
+  int temp = map(analogRead(A0),20,358,-40,125);
+  sendDataToThingSpeak(tempURL,temp);
+}
 
+void sendDataToThingSpeak(String URL, int value){
+  // Construire call/request HTTP
+  String httpPacket = "GET " + URL + String(value) + " HTTP/1.1\r\nHost: " + host + "\r\n\r\n";
+  int length = httpPacket.length();
+  
+  // Trimitem lungimea mesajului
+  Serial.print("AT+CIPSEND=");
+  Serial.println(length);
+  myDelay(10); //Adaugam un delay pentru ca ESP8266 sa aiba timp sa raspunda
+  // if(!Serial.find(">")) return -1;
+
+  // Trimitem request-ul HTTP 
+  Serial.print(httpPacket);
+  myDelay(10); //Adaugam un delay pentru ca ESP8266 sa aiba timp sa raspunda
+  if (!Serial.find("SEND OK\r\n")) return;
+
+}
+
+void setupTIMMER2(void){
   //Setarea Timer/Counter Control Register A in modul CTC
   //CTC - CLEAR TIMER ON COMPARE MATCH
-  //Reseteaza timerul cand ajunge la o anumita valoare care se afla in registrul OCR0A
-  TCCR0A=0b00000010;  
+  //Reseteaza timerul cand ajunge la o anumita valoare care se afla in registrul OCR2A
+    TCCR2A=0b00000010;  
 
-  //Asignam valoarea 11111001 care reprezinta 249 in decimal, registrului OCR0A
+  //Asignam valoarea 11111001 care reprezinta 249 in decimal, registrului OCR2A
   //Output Compare Register A
   //Valoarea este aleasa pentru a obtine 1ms impreuna cu prescalerul
-  OCR0A=0b11111001; 
+    OCR2A=0b11111001; 
   
   //Timer/Counter Intrerrupt Mask Register
   //Activarea tratarii intreruperilor generate de temporizatorul 0
-  //OCR0A genereaza o intrerupere atunci cand ajunge la valoarea 11111001 (249)
-  TIMSK0=0b00000010;   //Set the interrupt request
+  //OCR2A genereaza o intrerupere atunci cand ajunge la valoarea 11111001 (249)
+    TIMSK2=0b00000010;   //Set the interrupt request
   
   //Setam prescaler-ul la clock/64
-  TCCR0B=0b00000011; 
- }
+    TCCR2B=0b00000100; 
+  }
+
+void setup() {
+  setupESP8266();
+  setupTIMMER2();  
+  pinMode(13, OUTPUT);
+}
 
 //Implementarea proprie pentru functie de delay
 //Timpul este reprezentat in ms!
 void myDelay (int time){
   while(time > timerCounter ){
+    digitalWrite(13,HIGH); 
   }
-  if(timerCounter == time){
-    timerCounter=0;}
+  timerCounter=0;
 }
 
- void loop() {
-  // put your main code here, to run repeatedly:
+void loop() {
+ Serial.println("Am intrat din loop");
+ readTemp();
+ myDelay(10000);
+ digitalWrite(13,LOW); 
+  Serial.println("Am iesit din loop");
 }
 
 //Cand apare o cerere de intrerupere, timerCounter se incrementeaza
-//Timer/Counter0 Compare Match A (Vectorul numarul 15)
-ISR(TIMER0_COMPA_vect){ 
+//Timer/Counter2 Compare Match A (Vectorul numarul 15)
+ISR(TIMER2_COMPA_vect){ 
   timerCounter++;
 }
